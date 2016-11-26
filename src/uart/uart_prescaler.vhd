75a6c95fd1d9760c68826ece162e505d3953b2a6
Wojciech Pachuta <wojciech.pachuta01@gmail.com>
2016-11-26 18:02:42 +0100

    refactor prescaller

--- src/uart/uart_prescaler.vhd
+++ src/uart/uart_prescaler.vhd
@@ -4,17 +4,15 @@ use ieee.numeric_std.all;
 
 entity uart_prescaler is
 	port (
-		clk_in      : in    std_logic; --50 000 000
-		clk_out     : out   std_logic
+		clk_in      : in    std_logic; --50MHz
+		clk_out     : out   std_logic  --16 * 115200Hz
 	);
 end uart_prescaler;
 
 architecture uart_prescaler_impl of uart_prescaler is
-
-	signal tmp : std_logic := '0';
-
+	signal clk : std_logic := '0';
 begin
-
+	clk_out <= clk;
 	----9600
 	--process (clk_in) 
 	--	variable counter : Integer range 0 to 650 := 0;
@@ -42,18 +40,32 @@ begin
 	--end process;
 
 	--115200
+	--process (clk_in) 
+	--	variable counter : Integer range 0 to 26 := 0;
+	--begin	
+	--	if(rising_edge(clk_in)) then
+	--		case counter is
+	--			when 0 =>
+	--				clk_out <= '1';
+	--				counter := counter + 1;
+	--			when 13 =>
+	--				clk_out <= '0';
+	--				counter := counter + 1;
+	--			when 26 =>
+	--				counter := 0;
+	--			when others =>
+	--				counter := counter + 1;
+	--		end case;
+	--	end if;
+	--end process;
+
 	process (clk_in) 
-		variable counter : Integer range 0 to 26 := 0;
+		variable counter : Integer range 0 to 13 := 0;
 	begin	
 		if(rising_edge(clk_in)) then
 			case counter is
-				when 0 =>
-					clk_out <= '1';
-					counter := counter + 1;
 				when 13 =>
-					clk_out <= '0';
-					counter := counter + 1;
-				when 26 =>
+					clk <= not clk;
 					counter := 0;
 				when others =>
 					counter := counter + 1;
